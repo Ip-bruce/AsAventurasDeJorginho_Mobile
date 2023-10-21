439af4dd958821d7900c1146881c1ccee769c73b
Felipe Correia <61751940+Ip-bruce@users.noreply.github.com>
2023-10-21 17:56:28 -0300

    Character Animation Complete

--- As aventuras de Jorginho - mobile/Assets/_Project/Scripts/PlayerMovement.cs
+++ As aventuras de Jorginho - mobile/Assets/_Project/Scripts/PlayerMovement.cs
@@ -6,17 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     public float vel = 50;
     private float moveH,moveV;
+    public Animator anim;
     public static int colectables;
+    private bool walking = false;
+    //public SpriteRenderer playerSprite;
+    //public Sprite[] spriteList ;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         PlayerMove();
+       // PlayerAnimator();
     }
 
     void PlayerMove()
@@ -28,8 +33,18 @@ public class PlayerMovement : MonoBehaviour
 
         if(playerDirection != Vector2.zero)
         {
-            transform.Translate(playerDirection * vel);
+            transform.Translate(playerDirection * vel );
+            walking = true;
         }
+        else
+        {
+            walking = false;
+        }
+        
+
+        anim.SetFloat("Horizontal",moveH);
+        anim.SetFloat("vertical",moveV);
+        anim.SetBool("isWalking",walking);
     }
 
     //Player Colections
@@ -43,4 +58,20 @@ public class PlayerMovement : MonoBehaviour
         }
         Debug.Log("Colected!!!");
     }
+
+    // public void PlayerAnimator()
+    // {
+    //     if(moveH >= 1)
+    //     {
+    //         playerSprite.sprite = spriteList[1];
+    //     }
+    //     if(moveH == 0)
+    //     {
+    //         playerSprite.sprite = spriteList[0];
+    //     }
+    //     if(moveH < 0)
+    //     {
+    //         playerSprite.sprite = spriteList[2];
+    //     }
+    // }
 }
